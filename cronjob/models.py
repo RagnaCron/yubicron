221ca14e2a4b2f42ccaa2b676a6c7a76d0ef1dad
manuel.werder <manuel.werder@csbe.ch>
2019-10-08 10:59:17 +0200

    Added Model CronJob

--- cronjob/models.py
+++ cronjob/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CronJob(models.Model):
+	title = models.CharField(max_length=50)
+	url = models.URLField
+
+	needsAuthentication = models.BooleanField
+	userName = models.CharField(max_length=20)
+	password = models.CharField(max_length=30)
+
+	executionTime = models.CharField(max_length=30)
+
+	hasMailMessages = models.BooleanField
+	failMessage = models.TextField
+	successMessage = models.TextField
+	automaticJobStopperWhenToManyFailure = models.BooleanField
+
+	willSaveMessage = models.BooleanField
+	savedMessage = models.TextField
+
--- cronjob/views.py
+++ cronjob/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 
 def index(request):
+	# TODO: - POST
 	return render(request, 'cronjob/index.html', {})
