6dc808a677f7c9b9a9be250eac5fb8629aac52e2
manuel.werder <manuel.werder@csbe.ch>
2019-10-09 21:03:59 +0200

    user foreignKey

--- cronjob/models.py
+++ cronjob/models.py
@@ -1,22 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
 class CronJob(models.Model):
-	user = models.ForeignKey(on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	title = models.CharField(max_length=50)
-	url = models.URLField
+	url = models.URLField()
 
-	needs_authentication = models.BooleanField
+	needs_authentication = models.BooleanField()
 	username = models.CharField(max_length=20)
 	password = models.CharField(max_length=30)
 
 	execution_time = models.CharField(max_length=30)
 
-	fail_message = models.BooleanField
-	success_message = models.BooleanField
-	automatic_job_stopper_when_to_many_failures = models.BooleanField
+	fail_message = models.BooleanField()
+	success_message = models.BooleanField()
+	automatic_job_stopper_when_to_many_failures = models.BooleanField()
 
-	will_save_message = models.BooleanField
+	will_save_message = models.BooleanField()
