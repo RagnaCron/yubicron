80151fcf73ff7db4660b576fc9d0e19ac74969ab
manuel.werder <manuel.werder@csbe.ch>
2019-10-11 14:32:57 +0200

    deleted javascript

--- cronjob/static/scripts/hide.js
+++
@@ -1,8 +0,0 @@
-function displayHidden() {
-    if (getComputedStyle(document.getElementById("optionalLogin"), null).display == "none"){
-        document.getElementById("optionalLogin").style.display = "block";
-    }
-    else{
-        document.getElementById("optionalLogin").style.display = "none";
-    }
-}
