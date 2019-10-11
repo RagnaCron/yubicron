2ff4cee1ef965bcac7bd0d0e921764bd1c9bfa57
manuel.werder <manuel.werder@csbe.ch>
2019-10-11 16:50:05 +0200

    del

--- cronjob/test_views.py
+++ cronjob/test_views.py
@@ -34,17 +34,3 @@ class TestLoginView(TestCase):
 		self.assertTrue(response.status_code, 200)
 		response = self.client.get(reverse('cronjob:userLogout'))
 		self.assertTrue(response.status_code, 200)
-
-# class LogInTest(TestCase):
-#     def setUp(self):
-#         self.credentials = {
-#             'username': 'testuser',
-#             'password': 'secret'}
-#         User.objects.create_user(**self.credentials)
-#     def test_login(self):
-#         # login
-#         response = self.client.post('/login/', **self.credentials)
-#         # should be logged in now, fails however
-#         self.assertTrue(response.context['user'].is_active)
-
-
